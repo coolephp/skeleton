7dfa40289859e07be615e20fbc5ac7ef4c79edef
guanguans <ityaozm@gmail.com>
2020-11-24 13:46:24 +0800

    Add ServeCommand

---
+++ app/Command/ServeCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the coolephp/skeleton.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace App\Command;
+
+use Symfony\Component\Console\Input\InputOption;
+
+class ServeCommand extends \Guanguans\Coole\Console\Commands\ServeCommand
+{
+    protected $options = [
+        ['host', null, InputOption::VALUE_OPTIONAL, 'The host address to serve the application on', '127.0.0.1'],
+        ['port', null, InputOption::VALUE_OPTIONAL, 'The port to serve the application on', 8000],
+        ['docroot', null, InputOption::VALUE_REQUIRED, 'The docroot to serve the application on', './public/'],
+    ];
+}
