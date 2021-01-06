cd94e2e2278df52f7a09a73a8d272eb23fac0161
guanguans <ityaozm@gmail.com>
2021-01-06 14:28:26 +0800

    Update config files

--- config/app.php
+++ config/app.php
@@ -11,18 +11,39 @@ declare(strict_types=1);
  */
 
 return [
+    /*
+     * App 名称
+     */
     'name' => env('APP_NAME', 'Coole'),
 
+    /*
+     * 环境
+     */
     'env' => env('APP_ENV', 'production'),
 
+    /*
+     * Debug
+     */
     'debug' => env('APP_DEBUG', false),
 
+    /*
+     * 时区
+     */
     'timezone' => 'Asia/Shanghai',
 
+    /*
+     * .env 文件目录
+     */
     'env_path' => base_path(),
 
+    /*
+     * 配置文件目录
+     */
     'config_path' => base_path('config'),
 
+    /*
+     * 第三方服务
+     */
     'providers' => [
         // \App\Provider\ExampleServiceProvider::class,
     ],
@@ -34,6 +55,9 @@ return [
         // \App\Middleware\ExampleMiddleware::class,
     ],
 
+    /*
+     * 路由目录
+     */
     'route' => [
         base_path('route'),
     ],
