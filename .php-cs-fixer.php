7fa18ba04625e3cac8773de226c15aac14b968d5
yaozm <ityaozm@gmail.com>
2021-11-25 15:47:37 +0800

    Update github config files

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -12,6 +12,7 @@ $finder = Symfony\Component\Finder\Finder::create()
     ->in([
         __DIR__ . '/app',
         __DIR__ . '/config',
+        __DIR__ . '/database',
         __DIR__ . '/public',
         __DIR__ . '/route',
         __DIR__ . '/tests',
@@ -21,7 +22,7 @@ $finder = Symfony\Component\Finder\Finder::create()
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
         'header_comment' => ['header' => $header],
